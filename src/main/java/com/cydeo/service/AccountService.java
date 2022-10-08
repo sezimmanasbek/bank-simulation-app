c659eac8b3b0f81dfa3b795662e0bf728175775b
Sezi <sezimka1205@gmail.com>
2022-10-08 15:18:43 -0400

    bank simulation app start

---
+++ src/main/java/com/cydeo/service/AccountService.java
@@ -0,0 +1,16 @@
+package com.cydeo.service;
+
+import com.cydeo.enums.AccountType;
+import com.cydeo.model.Account;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+public interface AccountService {
+
+    Account createNewAccount(BigDecimal balance, Date creationDate, AccountType accountType, Long userId);
+
+    List<Account> listAllAccount();
+}
---
+++ src/main/java/com/cydeo/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,102 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.enums.AccountType;
+import com.cydeo.exception.AccountOwnershipException;
+import com.cydeo.exception.BadRequestException;
+import com.cydeo.exception.BalanceNotSufficientException;
+import com.cydeo.model.Account;
+import com.cydeo.model.Transaction;
+import com.cydeo.repository.AccountRepository;
+import com.cydeo.repository.TransactionRepository;
+import com.cydeo.service.TransactionService;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class TransactionServiceImpl implements TransactionService {
+
+    AccountRepository accountRepository;
+    TransactionRepository transactionRepository;
+
+    public TransactionServiceImpl(AccountRepository accountRepository,TransactionRepository transactionRepository) {
+        this.accountRepository = accountRepository;
+        this.transactionRepository = transactionRepository;
+    }
+
+    @Override
+    public Transaction makeTransfer(Account sender, Account receiver, BigDecimal amount, Date creationDate, String message) {
+        validateAccount(sender,receiver);
+        checkAccountOwnership(sender,receiver);
+        executeBalanceAndUpdateIfRequired(amount,sender,receiver);
+        /*
+        after all validations completed, and money is transferred, we need to create Transaction object and
+        create needed classes/method for this step, save the transaction
+         */
+
+        Transaction transaction = Transaction.builder().sender(sender.getId()).receiver(receiver.getId()).amount(amount)
+                .creationDate(new Date()).message(message).build();
+        transactionRepository.save(transaction);
+
+        return transaction;
+    }
+
+    private void executeBalanceAndUpdateIfRequired(BigDecimal amount, Account sender, Account receiver) {
+        if(checkSenderBalance(sender,amount)){
+            sender.setBalance(sender.getBalance().subtract(amount));
+            receiver.setBalance(receiver.getBalance().add(amount));
+        }else{
+            //not enough balance
+            throw new BalanceNotSufficientException("Balance is not enough for this transfer");
+        }
+    }
+
+    private boolean checkSenderBalance(Account sender, BigDecimal amount) {
+        return sender.getBalance().subtract(amount).compareTo(BigDecimal.ZERO) >= 0;
+    }
+
+    private void checkAccountOwnership(Account sender, Account receiver) {
+        /*
+         write an if statement that check if one of the account is saving,
+        and user if of sender or receiver is not the same, throw AccountOwnershipException
+         */
+
+        if((sender.getAccountType().equals(AccountType.SAVING) || receiver.getAccountType().equals(AccountType.SAVING)) &&
+        !sender.getUserId().equals(receiver.getUserId())){
+            throw new AccountOwnershipException("If one of the account is saving, userId must be the same");
+
+        }
+    }
+
+    private void validateAccount(Account sender, Account receiver) {
+    /*
+    - if any of the account is null
+    - if account ids are the same (same account)
+    - if the account exist in the database (repository)
+     */
+
+        if (sender == null || receiver == null){
+            throw new BadRequestException("Sender or Receiver cannot be null");
+        }
+
+        if(sender.getId().equals(receiver.getId())){
+            throw new BadRequestException("Sender account needs to be different than receiver");
+        }
+
+        findAccountById(sender.getId());
+        findAccountById(receiver.getId());
+
+    }
+
+    private Account findAccountById(UUID id) {
+       return accountRepository.findById(id);
+    }
+
+    @Override
+    public List<Transaction> findAllTransaction() {
+        return null;
+    }
+}
