ea1983c77264b5195224b581a2f9fedd107b6a3c
Sezi <sezimka1205@gmail.com>
2022-10-10 10:31:52 -0400

    Add testing

--- src/main/java/com/cydeo/BankSimulationAppApplication.java
+++ src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -1,14 +1,38 @@
 package com.cydeo;
 
+import com.cydeo.enums.AccountType;
+import com.cydeo.model.Account;
+import com.cydeo.service.AccountService;
+import com.cydeo.service.TransactionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @SpringBootApplication
 public class BankSimulationAppApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(BankSimulationAppApplication.class, args);
+        ApplicationContext container = SpringApplication.run(BankSimulationAppApplication.class, args);
+
+//        get account and transaction service bean
+        AccountService accountService = container.getBean(AccountService.class);
+        TransactionService transactionService = container.getBean(TransactionService.class);
+
+//        create 2 accounts sender and receiver
+        Account sender = accountService.createNewAccount(BigDecimal.valueOf(70),new Date(),AccountType.CHECKING,1L);
+        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50),new Date(),AccountType.SAVING,1L);
+
+        accountService.listAllAccount().forEach(System.out::println);
+
+        transactionService.makeTransfer(sender,receiver,new BigDecimal(20),new Date(),"Transaction 1");
+        transactionService.findAllTransaction().forEach(System.out::println);
+        accountService.listAllAccount().forEach(System.out::println);
+
+
     }
 
 }
