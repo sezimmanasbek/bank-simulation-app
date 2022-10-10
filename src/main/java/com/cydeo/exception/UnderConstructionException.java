4611f70ee70e5dd8b8b7200dda7872e53ffc4e57
Sezi <sezimka1205@gmail.com>
2022-10-10 10:40:15 -0400

    Add testing

---
+++ src/main/java/com/cydeo/exception/UnderConstructionException.java
@@ -0,0 +1,8 @@
+package com.cydeo.exception;
+
+public class UnderConstructionException extends RuntimeException {
+    public UnderConstructionException(String message) {
+        super(message);
+
+    }
+}
